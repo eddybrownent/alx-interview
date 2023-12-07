d604b121c0b66e626554311bd5b479380acf3ac6
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-12-07 09:41:53 +0300

    This method determines if all the boxes can be opened or not

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,27 +6,33 @@ A method that determines if all the boxes can be opened
 
 def canUnlockAll(boxes):
     """
-    Tell if all boxes can be opened.
+    Determine if all boxes can be opened.
 
     Args:
-        boxes (list of lists): Each box is numbered from 0 to (n - 1)
-                             each box may contain keys to the other boxes
+    - boxes (list of lists): Each box is numbered sequentially from 0 to n - 1,
+                             and each box may contain keys to the other boxes.
 
     Returns:
-        bool: True if all boxes can be opened, else False.
+    - bool: True if all boxes can be opened, else False.
     """
     if not boxes or type(boxes) is not list:
         return False
-    # opened boxes
-    opened_boxes = set()
-    # first box is unlocked
-    opened_boxes.add(0)
 
-    # Iterate through boxes
-    for box_num, keys in enumerate(boxes):
-        # If the current box is opened, add its keys to the set
-        if box_num in opened_boxes:
-            opened_boxes.update(keys)
+    # opened boxes
+    opened_boxes = set([0])
+
+    # List of new keys
+    new_keys = [0]
+
+    while new_keys:
+        current_box = new_keys.pop()
+
+        # Iterate through keys in the current box
+        for key in boxes[current_box]:
+            # If key opens new box and the box is not opened yet
+            if key < len(boxes) and key not in opened_boxes:
+                opened_boxes.add(key)
+                new_keys.append(key)
 
     # Check if all boxes have been opened
     return len(opened_boxes) == len(boxes)
